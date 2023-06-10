fcc7968f38f08d47af8cab0fad1983a1db77adf7
bleekerstudios <sofi@bleekerstudios.com>
2023-06-10 19:20:42 -0400

    logo

--- components/organisms/LogoList/index.tsx
+++ components/organisms/LogoList/index.tsx
@@ -16,7 +16,7 @@ const LogoList = () => {
         </div>
         <div className="">
           <Image
-            src={'/images/Salesforce-logo.svg'}
+            src={'/images/Salesforce.svg'}
             width={120}
             height="36"
             alt="forbes logo"
