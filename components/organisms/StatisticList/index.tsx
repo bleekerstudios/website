4be269932f7f3dde07155622e86306dfa49849dc
bleekerstudios <sofi@bleekerstudios.com>
2023-06-10 20:59:37 -0400

    Update index.tsx

--- components/organisms/StatisticList/index.tsx
+++ components/organisms/StatisticList/index.tsx
@@ -1,16 +1,16 @@
 import StatisticListItem from 'components/molecules/StatisticListItem'
 import { clear } from 'console'
 import React from 'react'
-import { FiBarChart, FiBriefcase, FiCloudLightning, FiFlag } from 'react-icons/fi'
+import { FiBarChart, FiBriefcase, FiCloudLightning, FiFastForward, FiFlag, FiTrendingUp } from 'react-icons/fi'
 
 const StatisticList = () => {
   return (
     <div className="w-full px-8 py-6 bg-light rounded flex flex-col gap-5 md:flex-row md:justify-around md:items-center">
-      <StatisticListItem icon={<FiBarChart />} label="NET-NEW SOLUTIONS DEPLOYED" value="67" />
+      <StatisticListItem icon={<FiTrendingUp />} label="SOLUTIONS DEPLOYED" value="67" />
       <div className="w-full h-[1px] bg-borderLight md:w-[1px] md:min-h-[65px]"></div>
       <StatisticListItem icon={<FiBriefcase />} label="CLIENTS" value="25" />
       <div className="w-full h-[1px] bg-borderLight md:w-[1px] md:min-h-[65px]"></div>
-      <StatisticListItem icon={<FiCloudLightning />} label="AVERAGE PROJECT LIFECYCLE" value="28 Days" />
+      <StatisticListItem icon={<FiFastForward />} label="AVERAGE PROJECT LIFECYCLE" value="28 Days" />
     </div>
   )
 }
