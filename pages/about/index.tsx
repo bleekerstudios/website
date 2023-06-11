31b4c37ea97c7bc3558f210a120fabc4a6b8be0f
bleekerstudios <sofi@bleekerstudios.com>
2023-06-11 11:06:30 -0400

    change

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -22,7 +22,7 @@ const About = () => {
               layout="fill"
               objectFit="contain"
               quality={80}
-              alt="Programmers when coding in their laptop"
+              alt=""
               
             />
           </div>
